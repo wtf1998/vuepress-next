5f830a871a3e40be47d70e0ef1bbe0dacfbaad90
Mr.Hope <mister-hope@outlook.com>
2022-09-25 16:41:13 +0800

    feat(bundler-vite): improve chunk name

--- packages/bundler-vite/src/plugins/mainPlugin.ts
+++ packages/bundler-vite/src/plugins/mainPlugin.ts
@@ -4,6 +4,24 @@ import autoprefixer from 'autoprefixer'
 import history from 'connect-history-api-fallback'
 import type { AliasOptions, Connect, Plugin, UserConfig } from 'vite'
 
+// https://github.com/rollup/rollup/blob/69ff4181e701a0fe0026d0ba147f31bc86beffa8/src/utils/sanitizeFileName.ts
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
+const sanitizeFileName = (name: string) => {
+  const driveLetter = DRIVE_LETTER_REGEX.exec(name)?.[0] || ''
+
+  return (
+    driveLetter +
+    name
+      .substring(driveLetter.length)
+      .replace(INVALID_CHAR_REGEX, '_')
+      .replace(/^_+/, '')
+  )
+}
+
 /**
  * The main plugin to compat vuepress with vite
  */
@@ -78,6 +96,7 @@ import '@vuepress/client/app'
             fs.readJsonSync(app.dir.client('package.json')).exports['./app']
           ),
           output: {
+            sanitizeFileName,
             ...(isServer
               ? {
                   // also add hash to ssr entry file, so that users could build multiple sites in a single process
